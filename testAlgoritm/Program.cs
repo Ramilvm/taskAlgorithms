423fbbd6120800dc417eceae52a65937faf65ea0
Ramilvm <ramilvm@code.edu.az>
2024-08-24 12:38:36 +0400

    tried to do home task

---
+++ testAlgoritm/Program.cs
@@ -0,0 +1,17 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//int i = 1;
+//    if ( i > 0 )
+//{
+//    Console.WriteLine("musbet");
+//}
+//    else
+//{
+//    Console.WriteLine("menfi");
+//}
+
+
+int i = 0;
+    if (i%3 && i%7)
